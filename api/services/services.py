b5364e6e6efc6618b5fdeef56bab0a7a7dbf7b7b
alejandrochmejia <alejandrochmejia@gmail.com>
2025-02-05 15:59:48 -0400

    data pasada a jsomn

--- api/services/services.py
+++ api/services/services.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
 from ..models.models import Album, Track, AlbumUpdate, TrackUpdate
-from ..data.data import albums
+import json
 
 def get_all_albums() -> List[Album]:
-    return albums
+    with open("api/data/data.json") as f:
+        base = json.load(f)
+    return base
